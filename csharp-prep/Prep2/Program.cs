628cc84337f8145e917bc5b17a12c1d0b1f97f83
Learnwell/wdd230 <mil21065@byui.edu>
2023-04-22 23:04:22 -0400

    Second Prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage" );
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        if (number >=90)
+        {
+            Console.WriteLine("grade A-Pass");
+        }
+        else if(number>=80)
+        {
+            Console.WriteLine("grade B-Pass");
+        }
+        else if(number>=70)
+        {
+            Console.WriteLine("grade C-Pass");
+        }
+        else if(number>=60)
+        {
+            Console.WriteLine("grade D-Pass");
+        }
+        else 
+        {
+            Console.WriteLine("grade F- Fail, Keep working");
+        }
+
+
+
+
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-    }
+        Console.WriteLine("What is your grade percentage?" );
+        string userInput  = Console.ReadLine();
+        int number  = int.Parse(userInput);
+
+        if  ( number >=90)
+        {       
+            Console.WriteLine("grade A-Pass");
+
+        }
+        else if (number>=80)
+        {
+            Console.WriteLine("grade B-Pass");
+        }
+
+        else if (number>=70)
+        {
+            Console.WriteLine("grade C-pass");
+        }
+
+        else if (number>=60)
+        {
+            Console.WriteLine("grade D-Pass");
+
+        }
+
+        else 
+        {
+            Console.WriteLine("grade F-Fail, keep working");
+        }
+         
+
+    }       
+ 
+        
 }
