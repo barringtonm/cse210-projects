2e3edc329f5291e81753c2a7dde2df08990393c3
Learnwell/wdd230 <mil21065@byui.edu>
2023-04-25 10:33:18 -0400

    Program.cs-prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,43 +2,51 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("What is the magic number == 6 ?");
-        Console.WriteLine("What is your guess?   ");
-        string userInput = Console.ReadLine();
-        int number =int.Parse(userInput);
+  static void Main(string[] args)
+  {
+        Console.WriteLine("magicNumber == 18");
 
-        if (number  == 4)
+        Console.WriteLine("What is the magicNumber?  ");
+
+        Console.WriteLine("What is your guess?  ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        
+        int guess = -1;
+        while(guess != magicNumber)
         {
-            Console.WriteLine("Higher");
+            Console.Write("What is your guess?  ");;
+            guess = int.Parse(Console.ReadLine());
+
+            if(magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
         }
 
-        else if ( number == 7)
-        {
-            Console.WriteLine("Lower");
-        }
+  }
 
-        else if ( number == 6)
-        {
-            Console.WriteLine("You guess it");
-        }
 
-        else
-        {
-            Console.WriteLine("Guess again");
-        }
-
-    }
-}
+            
+        
+       
+    
 
 
 
 
 
-
-
-
+}  
 
 
 
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,43 +1,60 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
-    
-    {
-        
-        Console.WriteLine("What is the magic number ==6 ?"   );
-        Console.WriteLine("What is your guess? "  );
-        string userInput  = Console.ReadLine();
-        int number  = int.Parse(userInput);
+   {
+        static void Main(string[] args)
 
-        if  ( number == 4)
-        {
-            Console.WriteLine("Higher");
-        }
-            
-
-        
-        else if (number > 6)
-        {
-            Console.WriteLine("Lower");
-        }
-
-        else if(number == 6)
-        {
-            Console.WriteLine("You guess it");
-        }
-        
-        
-            else
+         {  
+   
             {
-                Console.WriteLine("Guess again");
-                
-            }
-        
+                Console.WriteLine("What is the magic number == 6 ?");
+                Console.WriteLine("What is your guess?   ");
+                string userInput = Console.ReadLine();
+                int number =int.Parse(userInput);
+
+                if (number  == 4)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if ( number == 7)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if ( number == 6)
+                {
+                    Console.WriteLine("You guess it");
+                }
+
+                else
+                    {
+                    Console.WriteLine("Guess again");
+                    }
+
+            }        
     }
 
-         
- 
-        
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
