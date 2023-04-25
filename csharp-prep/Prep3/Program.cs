b12a3552de221d1b18c3a8b1b85258b93be75d46
Learnwell/wdd230 <mil21065@byui.edu>
2023-04-25 14:37:15 -0400

    Prep3 program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,32 +4,40 @@ class Program
 {
   static void Main(string[] args)
   {
-        Console.WriteLine("magicNumber == 18");
+        //Console.WriteLine("magicNumber == 18");
 
-        Console.WriteLine("What is the magicNumber?  ");
+        //Console.WriteLine("What is the magicNumber?  ");
 
-        Console.WriteLine("What is your guess?  ");
-        int magicNumber = int.Parse(Console.ReadLine());
+       // Console.WriteLine("What is your guess?  ");
+        //int magicNumber = int.Parse(Console.ReadLine());
         
-        int guess = 1;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        int guess = -1;
         while(guess != magicNumber)
         {
             Console.Write("What is your guess?  ");
             guess = int.Parse(Console.ReadLine());
 
             if(magicNumber > guess)
+
             {
                 Console.WriteLine("Higher");
             }
+
             else if (magicNumber < guess)
+
             {
                 Console.WriteLine("Lower");
 
             }
 
             else
+
             {
                 Console.WriteLine("You guessed it!");
+
             }
 
         }
@@ -39,16 +47,7 @@ class Program
 
             
         
-       
-    
-
-
-
-
-
-}  
-
-
+}
 
 
 
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,21 +5,48 @@ class Program
     static void Main(string[] args)
    
     {
-   
+        Random randomGenarator = new Random();
+        int magicNumber = randomGenerator.Next(1,11);
 
-        Console.WriteLine("What is the macic number?");
-        Console.WriteLine("What is the guess");
+        //Console.WriteLine("What is the macic number?");
+        //Console.WriteLine("What is the guess");
         int magicNumber = int.Parse(Console.ReadLine());
         
 
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1,11);
+       // Random randomGenerator = new Random();
+       // int number = randomGenerator.Next(1,11);
         
         int guess  = 1;
        
-       while (guess ! = magicNumber)
+       while (guess != magicNumber)
        {
-        
+        Console.Write("What is your guess");
+        guess = int.Parse(Console.ReadLine());
+
+        if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else if (magicNumber < guess)
+
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
        }
+
+
+
     }
+
+
+
+
+
 }
