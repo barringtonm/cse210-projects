c098f4042f6328d12ad15cd11a56736febdeb85f
Learnwell/wdd230 <mil21065@byui.edu>
2023-04-25 12:50:12 -0400

    minor change

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -11,10 +11,10 @@ class Program
         Console.WriteLine("What is your guess?  ");
         int magicNumber = int.Parse(Console.ReadLine());
         
-        int guess = -1;
+        int guess = 1;
         while(guess != magicNumber)
         {
-            Console.Write("What is your guess?  ");;
+            Console.Write("What is your guess?  ");
             guess = int.Parse(Console.ReadLine());
 
             if(magicNumber > guess)
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,23 @@ using System;
 class Program
 {
     static void Main(string[] args)
+   
     {
-        Console.WriteLine("Hello Prep4 World!");
+   
+
+        Console.WriteLine("What is the macic number?");
+        Console.WriteLine("What is the guess");
+        int magicNumber = int.Parse(Console.ReadLine());
+        
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+        
+        int guess  = 1;
+       
+       while (guess ! = magicNumber)
+       {
+        
+       }
     }
 }
