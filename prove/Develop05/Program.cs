db5d2960ca1f59477f1391267a75153260c74d4d
Learnwell/wdd230 <mil21065@byui.edu>
2023-05-04 20:27:59 -0400

    Program cs 5

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please enter your name?   ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number:  42 ");
+         string userInput = Console.ReadLine();
+         var a = int.Parse(userInput);
+
+          var square = a *a;
+          
+
+        Console.WriteLine($" {name}, the square number is {square} ");
+
+
+
+
+
     }
 }
