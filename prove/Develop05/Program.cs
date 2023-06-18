c5433ce679ab9b5a5a4bb049139a2f2574e2e08b
Learnwell/wdd230 <mil21065@byui.edu>
2023-06-18 08:50:08 -0400

    start dev05

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -6,17 +6,13 @@ class Program
     {
         Console.WriteLine("Welcome to the program!");
 
-        Console.Write("Please enter your name? ");
-        string name = Console.ReadLine();
 
-        Console.Write("Please enter your favorite number:  42 ");
-         string userInput = Console.ReadLine();
-         var a = int.Parse(userInput);
 
-          var square = a *a;
-          
 
-        Console.WriteLine($" {name}, the square number is {square} ");
+
+
+
+
 
 
 
